1af6d0f818dc5c87bd84ff05b87a3d819023ec25
Kailana Kahawaii <kailanakahawaii@gmail.com>
2020-01-16 21:45:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -343,8 +343,10 @@ long_name_steals_a_ton?
     end 
   end 
   most_steals = array.max 
-  find_player_by_steals(most_steals)
-  if player_with_longest_name == #found_name 
+  steals_a_ton =find_player_by_steals(most_steals)
+  if player_with_longest_name == steals_a_ton
+    return true 
+  end 
 end 
 
   
