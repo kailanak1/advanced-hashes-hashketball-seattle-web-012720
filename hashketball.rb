96910d047c9e48c7b71b22c9a6212c5ba37c0892
Kailana Kahawaii <kailanakahawaii@gmail.com>
2020-01-16 21:45:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -343,7 +343,7 @@ long_name_steals_a_ton?
     end 
   end 
   most_steals = array.max 
-  #make a method for find player name by steals 
+  find_player_by_steals(most_steals)
   if player_with_longest_name == #found_name 
 end 
 
